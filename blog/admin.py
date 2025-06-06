cb599a86a4cb5ca351fa2318d4ab3ddc09a924fb
Ekow Sackey <120583131+SS-ekow@users.noreply.github.com>
2025-06-06 01:23:59 +0000

    phase 3 i: admin intrface

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,49 @@
 from django.contrib import admin
 from .models import Post, Tag, Comment
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Tag)
-admin.site.register(Comment)
+# haram regen. apparently i need to make customizations to the admin experience.
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'color']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at', 'status']
+    list_filter = ['status', 'created_at', 'author']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created_at'
+    ordering = ['-created_at']
+
+    # this organizes the fields in the form 
+    fieldsets = (
+        ('Content', {
+            'fields': ('title', 'slug', 'content', 'excerpt')
+        }),
+        ('Publishing', {
+            'fields': ('status', 'published_at', 'author')
+        }),
+        ('Media & Tags', {
+            'fields': ('meta_image', 'tags'),
+            'classes': ('collapse',)  # Collapsible section
+        }),
+    )
+
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['short_content', 'author', 'post', 'created_at', 'is_reply']
+    list_filter = ['created_at', 'post']
+    search_fields = ['content', 'author__username']
+    
+    def short_content(self, obj):
+        return obj.content[:50] + "..." if len(obj.content) > 50 else obj.content
+    short_content.short_description = 'Content Preview'
+
+
+    # oh wow 
